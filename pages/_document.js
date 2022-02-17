d64dc8ac7daa9b87e51319431b712d7694f00543
kartikkoul <kartik.koul.01@gmail.com>
2022-02-17 17:43:53 +0530

    About Section begins🚀

--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,7 @@ class MyDocument extends Document {
             <Html>
                 <Head>
                     <link rel="icon" href="/favicon.ico" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
                 </Head>
                 <body>
                     <Main />
